40677ca61c3f625248dfead0ec55ee5881642140
LadyCodes <92242140+LadyCodes@users.noreply.github.com>
2021-10-11 17:07:58 +0700

    "initial login accomplished/ login maintainance ongoing"

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -4,39 +4,38 @@ import { ACCESS_TOKEN, TOKEN_CYBERSOFT } from "../utils/constants/config";
 
 //schema
 export const schema = yup.object().shape({
-    email: yup.string().required("Email is required!").email("Email is invalid!"),
-    password: yup
-      .string()
-      .required("Password is required!")
-      .min(6, "Password must have 6-8 characters!")
-      .max(8, "Password must have 6-8 characters!"),
-  });
-  
+  email: yup.string().required("Email is required!").email("Email is invalid!"),
+  password: yup
+    .string()
+    .required("Password is required!")
+    .min(6, "Password must have 6-8 characters!")
+    .max(8, "Password must have 6-8 characters!"),
+});
+
 //login axios
 class AuthService {
-    logIn(data){
-        return axios({
-            url: "http://jiranew.cybersoft.edu.vn/api/Users/signin",
-            method: "POST",
-            data,
-            TokenCybersoft: TOKEN_CYBERSOFT
-          });
-    }
+  logIn(data) {
+    return axios({
+      url: "http://jiranew.cybersoft.edu.vn/api/Users/signin",
+      method: "POST",
+      data,
+      TokenCybersoft: TOKEN_CYBERSOFT,
+    });
+  }
 }
 export default AuthService;
 
 //login maintainance axios
 class FetchMeService {
-  fetchMe(){
+  fetchMe() {
     return axios({
-      url:  'http://jiranew.cybersoft.edu.vn/api/Users/getUser',
-      method: 'POST',
-      headers:{
+      url: "http://jiranew.cybersoft.edu.vn/api/Users/getUser",
+      method: "POST",
+      headers: {
         Authorization: "Bearer " + localStorage.getItem(ACCESS_TOKEN),
-        TokenCybersoft: TOKEN_CYBERSOFT
-      }
-    })
+        TokenCybersoft: TOKEN_CYBERSOFT,
+      },
+    });
   }
-  
 }
-export {FetchMeService}
+export { FetchMeService };
